42afad2f8f479c61626d6037d3f81cc804014bc8
InaiaKenia <inaiakenia15@gmail.com>
2023-01-25 13:00:08 -0700

    updated About - format doc and clean code

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { FaSave } from "react-icons/fa";
 import "./About.css";
-//FaMailBulk, FaLinkedin, FaGithub
 
 // Functional component.
 const About = () => {
@@ -27,8 +26,8 @@ const About = () => {
           <li>JavaScript</li>
         </ul>
       ),
-    },    
-    
+    },
+
     {
       title: "RESUME",
       description: (
@@ -47,44 +46,9 @@ const About = () => {
     },
   ];
 
-
-    // {
-    //   title: "CONTACT",
-    //   description: (
-    //     <ul className="hide">
-    //       <li>
-    //         <FaMailBulk></FaMailBulk> inaiakenia15@gmail.com
-    //       </li>
-    //       <li>
-    //         <FaGithub></FaGithub>
-    //         <a
-    //           href="https://github.com/InaiaK"
-    //           className="github"
-    //           target="_blank"
-    //           rel="noreferrer"
-    //         >
-    //           GitHub
-    //         </a>
-    //       </li>
-    //       <li>
-    //         <FaLinkedin></FaLinkedin>
-    //         <a
-    //           href="https://www.linkedin.com/in/inaia-martin/"
-    //           className="linkedin"
-    //           target="_blank"
-    //           rel="noreferrer"
-    //         >
-    //           Linkedin
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   ),
-    // },
-
   const renderAbout = ({ title, description }, index) => {
     return (
       <Card key={`about-card-${index}`} className="about-box" bg="dark">
-        {/* <Card.Img className="about-img" src={img} alt={altText} /> */}
         <Card.Body className="card-body">
           {title}
           <Card.Text className="about-text">{description}</Card.Text>
